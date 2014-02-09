5ec0eaa82e6a083085c4f3e5ba9576e62ee20709
Jan-Piet Mens <jpmens@gmail.com>
2014-02-09 16:14:16 +0100

    add comments

--- sample-config.py
+++ sample-config.py
@@ -4,10 +4,11 @@ port = 1883
 username = None
 password = None
 
-# Pushover.net keys
+# Pushover.net User Key (on top right of their control panel)
 userkey = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
 # Map MQTT topics (wildcards allowed!) to Pushover.net app keys
+# (also called API Token/Key)
 topicmap = {
     'home/events/owntracks/+/+' : 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyy', # owntracks
     'home/t1'                   : 'zzzzzzzzzzzzzzzzzzzzzzzzzzzzzz', # icinga
