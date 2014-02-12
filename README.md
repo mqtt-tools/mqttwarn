c46aca46306afeba243bbe835f469dc00f5993d9
Ben Jones <ben.jones12@gmail.com>
2014-02-13 09:47:43 +1300

    Various changes...

--- README.md
+++ README.md
@@ -2,18 +2,10 @@
 
 This program subscribes to any number of MQTT topics (including wildcards) and publishes received payloads to [pushover.net](https://pushover.net).
 
-You associate topic branches to application keys (pushover terminology) in the configuration file (copy `sample-config.py` to `config.py` for use). For example:
+You associate topic branches to application keys (pushover terminology) in the configuration file (copy `mqtt2pushover.conf.sample` to `mqtt2pushover.conf` for use). 
 
-```python
-# Map MQTT topics (wildcards allowed!) to Pushover.net app keys
-topicmap = {
-    'home/events/owntracks/jpm/+' : 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyy', # owntracks
-    'home/monitoring/#'           : 'zzzzzzzzzzzzzzzzzzzzzzzzzzzzzz', # icinga
-    'oh/#'                        : 'mmmmmmmmmmmmmmmmmmmmmmmmmmmmmm', # openhab
-}
-```
+See details in the config sample for how to configure this script.
 
-A message published to, say, `home/t1` will use the `zzzz` app key for the pushover message, while a message published to `oh/warning` will use the `mmmm` app key. This allows this program to notify your Android or iOS devices on different services.
 
 ## Obligatory screenshot
 
--- mqtt2pushover.py
+++ mqtt2pushover.py
@@ -1,90 +1,112 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import paho.mqtt.client as paho   # pip install paho-mqtt
 from pushover import pushover # https://github.com/pix0r/pushover
-import ssl
-import sys
-import logging
 
-__author__    = 'Jan-Piet Mens <jpmens()gmail.com>'
+import paho.mqtt.client as paho   # pip install paho-mqtt
+import logging
+import sys, time
+
+__author__    = 'Jan-Piet Mens <jpmens()gmail.com>, Ben Jones <ben.jones12()gmail.com>'
 __copyright__ = 'Copyright 2014 Jan-Piet Mens'
 __license__   = """Eclipse Public License - v 1.0 (http://www.eclipse.org/legal/epl-v10.html)"""
+
+def on_connect(mosq, userdata, rc):
+    logging = userdata['logging']
+    logging.debug("Connected to MQTT broker")
+    for topic in userdata['topickey'].keys():
+        mqttc.subscribe(topic, 0)
 
 def on_message(mosq, userdata, msg):
     topic = msg.topic
     payload = str(msg.payload)
 
-    # Find the appkey for the particular topic of the message we just received
+    logging = userdata['logging']
+    logging.debug("Message received on %s: %s" % (topic, payload))
 
-    for sub in userdata['topicmap'].keys():
-        if paho.topic_matches_sub(sub, topic):
-            rhs = userdata['topicmap'][sub]
-            if type(rhs) is str:
-                userkey = userdata['userkey']
-                appkey = rhs
-            else:
-                userkey = rhs['userkey']
-                appkey = rhs['appkey']
+    recipients = userdata['topickey'][topic]
+    title = userdata['topictitle'][topic]
+    priority = userdata['topicpriority'][topic]
 
-            # logging.debug("Using userkey=%s, appkey=%s" % (userkey, appkey))
-            try:
-                pushover(message=payload, user=userkey, token=appkey)
-                logging.info("Posted [%s] to pushover from topic %s" % (str(payload), str(topic)))
-            except Exception, e:
-                logging.info("Cannot post to pushover: %s" % str(e))
-            break
+    for recipient in recipients:
+        userkey = userdata['pushoverkey'][recipient][0]
+        appkey = userdata['pushoverkey'][recipient][1]
 
+        logging.debug("Sending notification to %s..." % recipient)
+        try:
+            pushover(message=payload,
+                user=userkey, token=appkey,
+                title=title, priority=priority,
+                retry=60, expire=3600)
+            logging.debug("Successfully sent notification")
+        except Exception, e:
+            logging.warn("Notification failed: %s" % str(e))
 
 def on_disconnect(mosq, userdata, rc):
-    print "OOOOPS! disconnected"
+    logging = userdata['logging']
+    logging.debug("Connection to MQTT broker lost")
+    time.sleep(10)
 
-cf = {}
+if __name__ == '__main__':
 
-try:
-    execfile('config.py', cf)
-except Exception, e:
-    print "Cannot load config.py: %s" % str(e)
-    sys.exit(2)
+    # load configuration
+    conf = {}
+    try:
+        execfile('mqtt2pushover.conf', conf)
+    except Exception, e:
+        print "Cannot load mqtt2pushover.conf: %s" % str(e)
+        sys.exit(2)
 
-LOGFILE = cf.get('logfile', 'logfile')
-LOGFORMAT = '%(asctime)-15s %(message)s'
-DEBUG=True
+    # initialise logging    
+    logfile = conf['logfile']
+    loglevel = conf['loglevel']
+    logformat = '%(asctime)-15s %(message)s'
 
-if DEBUG:
-    logging.basicConfig(filename=LOGFILE, level=logging.DEBUG, format=LOGFORMAT)
-else:
-    logging.basicConfig(filename=LOGFILE, level=logging.INFO, format=LOGFORMAT)
+    logging.basicConfig(filename=logfile, level=loglevel, format=logformat)
+    logging.info("mqtt2pushover started")
+    logging.debug("DEBUG MODE")
 
-logging.info("Starting")
-logging.debug("DEBUG MODE")
+    # create the user data to pass through to our MQTT listeneres
+    userdata = {
+        'logging'       : logging,
+        'pushoverkey'   : conf['pushoverkey'],
+        'topickey'      : conf['topickey'],
+        'topictitle'    : conf['topictitle'],
+        'topicpriority' : conf['topicpriority'],
+    }
 
-userdata = {
-    'userkey' : cf['userkey'],
-    'topicmap' : cf['topicmap'],
-}
+    # initialise the MQTT broker connection
+    mqttc = paho.Client('mqtt2pushover', clean_session=False, userdata=userdata)
+    mqttc.on_connect = on_connect
+    mqttc.on_message = on_message
+    mqttc.on_disconnect = on_disconnect
 
-mqttc = paho.Client('mqtt2pushover', clean_session=False, userdata=userdata)
-mqttc.on_message = on_message
-mqttc.on_disconnect = on_disconnect
+    # check for authentication
+    username = conf['username']
+    if username is not None:
+        password = conf['password']
+        mqttc.username_pw_set(username, password)
 
-if cf['username'] is not None:
-    mqttc.username_pw_set(cf['username'], cf['password'])
+    # configure the last-will-and-testament
+    lwt = conf['lwt']
+    mqttc.will_set(lwt, payload="mqtt2pushover", qos=0, retain=False)
 
-try:
-    mqttc.connect(cf['broker'], int(cf['port']), 60)
-except:
-    logging.info("Can't connect to MQTT on %s:%d" % (cf['broker'], cf['port']))
+    # attempt to connect to the MQTT broker
+    broker = conf['broker']
+    port = int(conf['port'])
+    try:
+        mqttc.connect(broker, port, 60)
+    except Exception, e:
+        logging.error("Failed to connect to MQTT broker on %s:%d: %s" % (broker, port, str(e)))
+        sys.exit(2)
 
-for topic in cf['topicmap'].keys():
-    logging.debug("Subscribing to %s" % topic)
-    mqttc.subscribe(topic, 0)
-
-try:
-    mqttc.loop_forever()
-except KeyboardInterrupt:
-    mqttc.loop_stop()
-    mqttc.disconnect()
-    sys.exit(0)
-except:
-    raise
+    # start listening...
+    try:
+        mqttc.loop_forever()
+    except KeyboardInterrupt:
+        mqttc.loop_stop()
+        mqttc.disconnect()
+        sys.exit(0)
+    except Exception, e:
+        logging.error("Error on MQTT broker connection: %s" % str(e))
+        sys.exit(2)
