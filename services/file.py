2662240bdbbfc1c5e4887969480e0544e6012020
Jan-Piet Mens <jpmens@gmail.com>
2014-02-15 16:19:16 +0100

    add in-module doc

--- services/file.py
+++ services/file.py
@@ -9,15 +9,18 @@ def plugin(srv, item):
 
     srv.logging.debug("*** MODULE=%s: service=%s, target=%s", __file__, item.service, item.target)
 
+    # item.config is brought in from the configuration file
     config   = item.config
 
-
-# FIXME: remove next line
-    print item.data
-
+    # addrs is a list[] associated with a particular target.
+    # While it may contain more than one item (e.g. pushover)
+    # the `file' service carries one only, i.e. a path name
     filename = item.addrs[0]
 
+    # If the incoming payload has been transformed, use that,
+    # else the original payload
     text = item.get('message', item.payload)
+
     if type(config) == dict and 'append_newline' in config:
         text = text + "\n"
 
