d9d7cfec64f40783d842336f5ba2f3c354b1a253
Ben Jones <ben@weka>
2014-02-17 08:31:20 +1300

    Improved JSON handling in XBMC plugin

--- services/xbmc.py
+++ services/xbmc.py
@@ -7,6 +7,7 @@ __license__   = """Eclipse Public License - v 1.0 (http://www.eclipse.org/legal/
 
 import urllib
 import urllib2
+import json
 
 def plugin(srv, item):
 
@@ -16,12 +17,21 @@ def plugin(srv, item):
     title    = item.title
     message  = item.message
 
-    command = '{"jsonrpc":"2.0","method":"GUI.ShowNotification","params":{"title":"%s","message":"%s"},"id":1}' % (title, message)
-    command = command.encode('utf-8')
+    jsonparams = {
+        "jsonrpc" : "2.0",
+        "method"  : "GUI.ShowNotification",
+        "id"      : 1,
+        "params"  : { 
+            "title"   : title, 
+            "message" : message 
+        }
+    }
+    jsoncommand = json.dumps(jsonparams)
+
     url = 'http://%s/jsonrpc' % (xbmchost)
     try:
         srv.logging.debug("Sending XBMC notification to %s [%s]..." % (item.target, xbmchost))
-        req = urllib2.Request(url, command)
+        req = urllib2.Request(url, jsoncommand)
         req.add_header("Content-type", "application/json")
         response = urllib2.urlopen(req)
         srv.logging.debug("Successfully sent XBMC notification")
