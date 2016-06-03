43bf49046908ac36a165a72c66c58edce057e9db
Andreas Motl <andreas.motl@elmyra.de>
2016-06-03 19:03:49 +0200

    Make Hiveeyes sensor machinery production grade

--- examples/hiveeyes/hiveeyes.py
+++ examples/hiveeyes/hiveeyes.py
@@ -4,7 +4,8 @@
 import re
 import json
 from datetime import datetime
-from pprint import pformat
+from pprint import pformat, pprint
+from collections import deque, defaultdict
 
 
 # ------------------------------------------
@@ -35,29 +36,53 @@ monitoring_rules = {
 
     # Let's trigger an alarm on a weight loss
     # of 750g or more between two measurements
-    'wght2':  0.75,
+    'wght2':  {'threshold': 0.75, 'unit': 'kilo'},
 
     # For testing the machinery by
     # monitoring a clock signal
-    'second': 0.5,
+    'second': {'threshold': 0.5, 'unit': 'second'},
     }
 
 # Trigger a data-loss event when not receiving any fresh
 # data after 20 minutes from a device already seen before.
 data_loss_timeout = 20 * 60
 
+# Just a hack to give the anonymous data source of "Labhive One" a better name
+dashboard_aliases = {
+    '25a0e5df-9517-405b-ab14-cb5b514ac9e8': 'hiveeyes-labs-wedding',
+}
+
 
 # ------------------------------------------
 #                   Main
 # ------------------------------------------
 
-# A dictionary for remembering measurement values
-# across a window of two value elements.
-# TODO: Add "network/gateway" information to all bookkeeping data to enable multi-tenancy
-history = dict()
-history_before = dict()
-last_measurement_times = {}
-data_loss_state = {}
+# Data structure for remembering measurement values across a variable window size.
+class HistoricData(object):
+
+    # How many data points to remember
+    backlog = 20
+
+    def __init__(self):
+        series_factory = lambda: deque(maxlen=HistoricData.backlog)
+
+        # Some data containers for remembering ...
+
+        # Inbound data payload fragments
+        self.fragments = defaultdict(series_factory)
+
+        # All attributes aggregated across consecutive data payload fragments
+        # This is actually used for threshold comparison
+        self.history = defaultdict(series_factory)
+
+        # Arbitrary node states like "data-loss"
+        self.states = defaultdict(dict)
+
+        # Most recent moment of data acquisition per node
+        self.moments = {}
+
+# A single data container instance to rule them all
+hdata = HistoricData()
 
 
 def format_passthrough(data):
@@ -89,10 +114,65 @@ def hiveeyes_more_data(topic, data, srv):
     """
     Add more data to transformation data object, used later when formatting the outgoing message.
     """
+
+    if not topic.endswith('message-json'):
+        return
+
+    message = data['payload']
+
+    # Decode message (redundant with "hiveeyes_schwarmalarm_filter")
+    mdata = dict(json.loads(message).items())
+    tdata = mdata.copy()
+    tdata.update(hiveeyes_topic_to_topology(topic))
+
+    # Compute bookkeeping key for multi-tenancy
+    origin = '{realm}/{network}/{gateway}/{node}'.format(**tdata)
+
+    condition = {'current': None, 'previous': None}
+
+    # Get current and previous measurements
+    fragments = condition.copy()
+    try:
+        fragments['current'] = hdata.fragments[origin][-1]
+    except IndexError:
+        pass
+    try:
+        fragments['previous'] = hdata.fragments[origin][-2]
+    except IndexError:
+        pass
+
+    history = condition.copy()
+    try:
+        history['current'] = hdata.history[origin][-1]
+    except IndexError:
+        pass
+    try:
+        history['previous'] = hdata.history[origin][-2]
+    except IndexError:
+        pass
+
+    # Compute Grafana dashboard name from simple map, fall back
+    # to network identifier as this is the default of Kotori's GrafanaManager.
+    dashboard = dashboard_aliases.get(tdata['network'], tdata['network'])
+
     more_data = {
-        'current': pformat(json.loads(data['payload'])),
-        'history': pformat(history_before),
+
+        # current/previous data
+        'fragments': fragments,
+        'history': history,
+
+        # more enrichment
+        'dashboard': dashboard,
+
+        # utilities
+        'pformat': pformat,
     }
+
+    # Also push the "event" state into the template machinery
+    # This is used for displaying a preformatted description message to humans
+    if 'event' in hdata.states[origin]:
+        more_data.update(hdata.states[origin]['event'])
+
     return more_data
 
 
@@ -102,43 +182,66 @@ def hiveeyes_schwarmalarm_filter(topic, message):
     to trigger notification only if delta is greater threshold.
     Also, perform data-loss bookkeeping.
     """
-    global history, history_before
 
     if not topic.endswith('message-json'):
         return True
 
-    # Decode message
-    data = dict(json.loads(message).items())
-    data.update(hiveeyes_topic_to_topology(topic))
+    # Decode message (redundant with "hiveeyes_more_data")
+    mdata = dict(json.loads(message).items())
+    tdata = mdata.copy()
+    tdata.update(hiveeyes_topic_to_topology(topic))
+
+    # Compute bookkeeping key for multi-tenancy
+    origin = '{realm}/{network}/{gateway}/{node}'.format(**tdata)
 
     # Data-loss bookkeeping
-    origin = '{realm}/{network}/{gateway}/{node}'.format(**data)
-    last_measurement_times[origin] = datetime.utcnow()
+    hdata.moments[origin] = datetime.utcnow()
 
-    # Remember current history data for later access from hiveeyes_more_data
-    history_before = history.copy()
+    # Get most recent measurement from series
+    try:
+        history = hdata.history[origin][-1].copy()
+    except IndexError:
+        history = {}
 
     alarm = False
     for field in monitoring_rules.keys():
 
         # Skip if monitored field is not in data payload
-        if field not in data:
+        if field not in tdata:
             continue
 
         # Read current value and appropriate threshold
-        current = data[field]
-        threshold = monitoring_rules[field]
+        current = tdata[field]
+        threshold = monitoring_rules[field]['threshold']
 
         # Compare current with former value and set
         # semaphore if delta is greater threshold
         if field in history:
-            former = history[field]
-            delta = current - former
-            if abs(delta) >= threshold:
+            previous = history[field]
+            delta = float(current) - float(previous)
+            delta_nosign = abs(delta)
+            if delta_nosign >= threshold:
+
+                # Set event state outcome / semaphore
                 alarm = True
 
+                # Format description message for displaying to humans, e.g.
+                # »Sensor value "wght2" gained 1.42 kilos.«
+                unit = monitoring_rules[field].get('unit', 'point')
+                verb = delta != 0 and (cmp(delta, 0) == 1 and 'gained' or 'lost') or 'did not change'
+                description = 'Sensor value "{field}" {verb} {delta_nosign} {unit}s.'.format(
+                    field=field, verb=verb, delta_nosign=delta_nosign, unit=unit)
+                hdata.states[origin]['event'] = {'field': field, 'delta': delta, 'description': description}
+            else:
+                # Reset event state
+                if 'event' in hdata.states[origin]:
+                    del hdata.states[origin]['event']
+
     # Remember current values for next round
-    history = data.copy()
+    hdata.fragments[origin].append(mdata)
+
+    history.update(mdata)
+    hdata.history[origin].append(history)
 
     # The filter function should return True if the message should
     # be suppressed, or False if the message should be processed.
@@ -167,31 +270,31 @@ def hiveeyes_dataloss_monitor(srv):
 
     # List of all seen data origins (devices).
     # The identifiers are made of essential parts of the MQTT topic.
-    origins = last_measurement_times.keys()
+    origins = hdata.moments.keys()
 
     # Iterate all seen devices
     for origin in origins:
 
         # Signal no data loss as a default
-        data_loss_state.setdefault(origin, False)
+        hdata.states[origin].setdefault('data-loss', False)
 
         # Get timestamp of last valid measurement
-        last_measurement_time = last_measurement_times[origin]
+        last_moment = hdata.moments[origin]
 
         # Compute duration since last valid measurement
-        delta = now - last_measurement_time
+        delta = now - last_moment
 
         # Check if duration is longer than user-defined threshold
         if delta.total_seconds() >= data_loss_timeout:
 
             # Only act if not already being in state of data loss
-            if not data_loss_state[origin]:
+            if 'data-loss' not in hdata.states[origin] or not hdata.states[origin]['data-loss']:
 
                 # Set data loss state
-                data_loss_state[origin] = True
+                hdata.states[origin]['data-loss'] = True
 
                 # Send out data loss notification
-                data = {'description': 'Detected data loss'}
+                data = {'description': 'Detected data loss.'}
                 send_to_targets(
                     section = 'hiveeyes-schwarmalarm',
                     topic   = '{origin}/message-json'.format(origin=origin),
@@ -199,7 +302,7 @@ def hiveeyes_dataloss_monitor(srv):
 
         else:
             # Reset data loss state
-            data_loss_state[origin] = False
+            hdata.states[origin]['data-loss'] = False
 
     # TODO: Republish data loss notification to MQTT bus
     #srv.mqttc.publish('data-loss-topic'.format(**locals()), 'data-loss')
@@ -225,3 +328,4 @@ def load_module(path):
 # Mitigate "AttributeError: '_ssl._SSLSocket' object has no attribute 'issuer'"
 service_xmpp = load_module('services/xmpp.py')
 service_xmpp.xmpppy_monkeypatch_ssl()
+
