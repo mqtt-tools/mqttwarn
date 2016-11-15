efe1d1bdea9e9c038a446c3ea7fd2f44bdbd4ac4
przemas <przemas@apache>
2016-11-15 18:16:38 +0100

    fbchat rework

--- services/fbchat.py
+++ services/fbchat.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__author__    = 'Przemek Anuszek <przemas75()gmail.com>'
-__copyright__ = 'Copyright 2016 Przemyslaw Anuszek'
+__author__    = 'Jan-Piet Mens <jpmens()gmail.com>'
+__copyright__ = 'Copyright 2014 Jan-Piet Mens'
 __license__   = """Eclipse Public License - v 1.0 (http://www.eclipse.org/legal/epl-v10.html)"""
 
 import fbchat
@@ -12,29 +12,22 @@ def plugin(srv, item):
 
     srv.logging.debug("*** MODULE=%s: service=%s, target=%s", __file__, item.service, item.target)
 
-#    client = fbchat.Client("YOUR_ID", "YOUR_PASSWORD")
-    fbchat_keys = item.addrs
+    client = item.addrs[0]
+    password = item.addrs[1]
+    friend = item.addrs[2]
 
-    fbclient = fbchat.Client(
-        username        = fbchat_keys[0],
-        password        = fbchat_keys[1]
-    )
-    fbfriend = fbclient.getUsers(
-        friend          = fbchat_keys[2]
-    )
-    #friends = client.getUsers("FRIEND'S NAME")  # return a list of names
-    #friend = friends[0]
+    fbclient = fbchat.Client(client, password)
+    friends = fbclient.getUsers(friend)
+    ffriend = friends[0]
+    srv.logging.debug("user %s" % (item.target))
 
-    text = item.message[0:138]
+    text = item.message
     try:
         srv.logging.debug("Sending msg to %s..." % (item.target))
-        sent = fbclient.send(fbfriend, text)
-        srv.logging.debug("Successfully sent tweet")
-#    except twitter.TwitterError, e:
-#        srv.logging.error("FbChatError: %s" % (str(e)))
-#        return False
+        sent = fbclient.send(ffriend.uid, text)
+        srv.logging.debug("Successfully sent message")
     except Exception, e:
-        srv.logging.error("Error sending tweet to %s: %s" % (item.target, str(e)))
+        srv.logging.error("Error sending fbchat to %s: %s" % (item.target, str(e)))
         return False
 
     return True
