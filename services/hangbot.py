b5b8e3eba3296d1040a1f5eb283a5284606cea8c
jusplainmike <jusplainmike@gmail.com>
2018-02-11 04:12:10 -0600

    Update hangbot.py
    Fixed copyright and logging

--- services/hangbot.py
+++ services/hangbot.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+'''
 __author__    = 'Bram Hendrickx'
 __copyright__ = 'Copyright 2016 Bram Hendrickx'
 __license__   = """Eclipse Public License - v 1.0 (http://www.eclipse.org/legal/epl-v10.html)"""
 
-''' Modified ifttt.py to work with hangoutsbot api plugin https://github.com/hangoutsbot/hangoutsbot/wiki/API-Plugin '''
+Original script by Bram Hendrickx. https://github.com/jpmens/mqttwarn/blob/master/services/ifttt.py
+Modified to work with hangoutsbot api plugin https://github.com/hangoutsbot/hangoutsbot/wiki/API-Plugin
+
+'''
+__author__    = 'Michael Brougham'
+__copyright__ = 'Copyright 2018 Michael Brougham'
+__license__   = """Eclipse Public License - v 1.0 (http://www.eclipse.org/legal/epl-v10.html)"""
+
 
 import json
 import requests
@@ -25,9 +32,9 @@ def plugin(srv, item):
         url = "https://" + item.addrs[0] + ":" + item.addrs[1]
         headers = {'content-type': 'application/json'}
         requests.post(url, data = json.dumps(payload), headers = headers, verify=False)
-        srv.logging.debug("Successfully sent ifttt event")
+        srv.logging.debug("Successfully sent to hangoutsbot")
     except Exception, e:
-        srv.logging.warning("Cannot send ifttt event: %s" % (str(e)))
+        srv.logging.warning("Failed to send message to hangoutsbot" % (str(e)))
         return False
 
     return True
