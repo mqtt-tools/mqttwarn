9d888b8daaeb5d801ce8f59161b28d3615549949
psyciknz <github@andc.co.nz>
2019-09-10 08:26:44 +1200

    added run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+#If you have any extra requirements for local services, add the pip commands here eg:
+#pip install asterisk-ami
+ 
+
+#leave the last line alone as this runs mqttwarn.
+python mqttwarn.py
