a8b9987ccf8dc645005b389f1a1d210ce913ff16
Andreas Motl <andreas.motl@panodata.org>
2021-06-15 20:53:02 +0200

    Tests: Refactor configuration files, improve naming things

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,10 +2,10 @@
 # (c) 2018 The mqttwarn developers
 
 # Configuration- and function files used by the test harness
-configfile = 'tests/selftest.ini'
-configfile_v2 = 'tests/selftest_v2.ini'
+configfile_full = 'tests/etc/full.ini'
+configfile_service_loading = 'tests/etc/service-loading.ini'
 configfile_no_functions = 'tests/etc/no-functions.ini'
 configfile_empty_functions = 'tests/etc/empty-functions.ini'
-configfile_bad_functions = 'tests/etc/bad-functions.ini'
-funcfile = 'tests/selftest.py'
-bad_funcfile = 'tests/bad_funcs.py'
+configfile_unknown_functions = 'tests/etc/unknown-functions.ini'
+funcfile_good = 'tests/etc/functions_good.py'
+funcfile_bad = 'tests/etc/functions_bad.py'
