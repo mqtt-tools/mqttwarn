06b347214e8f1da7dee7b3689f5624722019b1a6
Andreas Motl <andreas.motl@panodata.org>
2021-10-16 03:01:00 +0200

    Add software tests for "file" service

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,2 +1,12 @@
+import pytest
+
 # Import fixtures
 from mqttwarn.testing.fixtures import mqttwarn_service as srv  # noqa
+
+@pytest.fixture
+def fake_filesystem(fs):  # pylint:disable=invalid-name
+    try:
+        fs.create_dir("/tmp")
+    except:
+        pass
+    yield fs
