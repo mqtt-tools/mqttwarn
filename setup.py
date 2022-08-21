bb61b8709397d379c1f14aa3ec7f5b42349b8d03
Andreas Motl <andreas.motl@panodata.org>
2022-08-22 00:03:44 +0200

    Bump version: 0.28.1 → 0.29.0

--- setup.py
+++ setup.py
@@ -160,7 +160,7 @@ extras["test"] = [
 
 
 setup(name='mqttwarn',
-      version='0.28.1',
+      version='0.29.0',
       description='mqttwarn - subscribe to MQTT topics and notify pluggable services',
       long_description=README,
       license="EPL 2.0",
