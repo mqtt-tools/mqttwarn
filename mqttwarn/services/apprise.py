cf52fb7eb9cc20b9f2aca948b7d590b311c2d23d
Andreas Motl <andreas.motl@panodata.org>
2023-02-13 13:55:11 +0100

    [Apprise] Add deprecation warning about outdated usage

--- mqttwarn/services/apprise.py
+++ mqttwarn/services/apprise.py
@@ -1,1 +1,7 @@
+import warnings
+
 from mqttwarn.services.apprise_single import plugin
+
+warnings.warn("`mqttwarn.services.apprise` will be removed in a future release of mqttwarn. "
+              "Please use `mqttwarn.services.apprise_single` or `mqttwarn.services.apprise_multi` instead.",
+              category=DeprecationWarning)
