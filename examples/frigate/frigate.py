37adcb9cac6e00f8b0bed65a646e01edb090f863
Andreas Motl <andreas.motl@panodata.org>
2023-04-12 19:09:22 +0200

    Forwarding Frigate events to Ntfy: Add baseline material and docs

---
+++ examples/frigate/frigate.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+def frigate_events(topic, data, srv=None):
+    if srv is not None:
+        srv.logging.debug('events ******************************************')
+    a = json.loads(data['payload'])['after']
+    f = lambda x: (y.replace('_', ' ') for y in x)
+    r = {
+      'camera': a['camera'],
+      'label':  a['sub_label'] or a['label'],
+      'current_zones': ', '.join(f(a['current_zones'])),
+      'entered_zones': ', '.join(f(a['entered_zones'])),
+      'time':   datetime.fromtimestamp(a['frame_time'])
+    }
+    r.update({
+      'title':  f"{r['label']} entered {r['entered_zones']}",
+      'format': f"In zones {r['current_zones']} at {r['time']}",
+      'click':  f"https://frigate/events?camera={r['camera']}&label={r['label']}&zone={a['entered_zones'][0]}"
+    })
+    return r
+
+def frigate_events_filter(topic, message, section, srv=None):
+    if srv is not None:
+        srv.logging.debug('filter ******************************************')
+    try:
+        message = json.loads(message)
+    finally:
+        message = None
+    if message and message.get('type', None) != 'end' and 'after' in message:
+        a = message['after']
+        return False not in (x in a and (x == 'current_zones' or a[x]) for x in
+                                  ('false_positive', 'camera', 'label',
+                                   'current_zones', 'entered_zones',
+                                   'frame_time'))
+    return False
