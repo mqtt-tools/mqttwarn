4e647061ce79fbd20a2bc2701a2bb587935ee7d8
Andreas Motl <andreas.motl@panodata.org>
2023-04-13 06:34:58 +0200

    Docs: Use `furo` theme, add mqttwarn logo, add `myst_parser` extension

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,10 @@ author = 'The mqttwarn authors'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    # https://github.com/executablebooks/MyST-Parser
+    "myst_parser",
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -23,5 +26,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'furo'
 html_static_path = ['_static']
+html_logo = "mqttwarn-logo.png"
